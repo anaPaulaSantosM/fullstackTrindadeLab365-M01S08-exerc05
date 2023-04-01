9c64779e9a05e8bb6ca4c44ff698839e70657101
Ana Paula dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-31 22:47:41 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+(async () => {
+
+    const database = require('./db');
+    const Equipamento = require('./equipamento');
+    await database.sync();
+
+    /*const novoEquipamento = await Equipamento.create({
+        nome: 'Corda',
+        preco: 150          
+    })
+
+    console.log(novoEquipamento);*/
+    
+    //const equipamentos = await Equipamento.findAll();
+
+    const equipamentos = await Equipamento.findByPk(2);
+    console.log(equipamentos);
+
+    equipamentos.preco = 200
+    await equipamentos.save();
+
+})();
